83590a455468ee5aeaf1b1e10fecd1a626fc14e6
engelmi <engel.michael@hotmail.de>
2022-01-07 17:07:01 +0100

    removed unnecessary use of fmt.Sprintf

--- internal/producer/request.go
+++ internal/producer/request.go
@@ -45,7 +45,7 @@ func decodeJsonBody(w http.ResponseWriter, r *http.Request, dst interface{}) err
 			return &malformedRequest{status: http.StatusBadRequest, msg: msg}
 
 		case errors.Is(err, io.ErrUnexpectedEOF):
-			msg := fmt.Sprintf("Request body contains badly-formed JSON")
+			msg := "Request body contains badly-formed JSON"
 			return &malformedRequest{status: http.StatusBadRequest, msg: msg}
 
 		case errors.As(err, &unmarshalTypeError):
